1883ef37b1db9a9b38f40091b6930ec60f4fd3e8
Olga Polishchuk <olgapolichuk@ukr.net>
2023-01-15 22:24:39 +0200

    add page up scroll adn metateg OG

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -36,3 +36,12 @@ var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.cal
       }, false);
     });
 }
+
+// Get the element
+let topBtn = document.querySelector(".page-up");
+
+// On Click, Scroll to the page's top, replace 'smooth' with 'auto' if you don't want smooth scrolling
+topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+
+// On scroll, Show/Hide the btn with animation
+window.onscroll = () => window.scrollY > 400 ? topBtn.style.opacity = 1 : topBtn.style.opacity = 0
