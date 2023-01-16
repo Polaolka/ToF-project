9ee3cb8d2bff47b3fe0783fd82434dee99913c16
Polaolka <101866764+Polaolka@users.noreply.github.com>
2023-01-16 12:05:55 +0000

    Deploying to gh-pages from @ Polaolka/tof-project@04ed4fbfa6386eaab3c8f612e12f3ca9160b17ed 🚀

---
+++ index.c9de51dd.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector(".page-up");o.onclick=function(){return window.scrollTo({top:0,behavior:"smooth"})},window.onscroll=function(){return window.scrollY>400?o.style.opacity=1:o.style.opacity=0}}();
+//# sourceMappingURL=index.c9de51dd.js.map
