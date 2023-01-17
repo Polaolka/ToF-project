4c77b3ae24dafd9af1556ca3ee233e43c0efbb57
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2023-01-17 12:59:04 +0200

    fix slider

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,41 +1,27 @@
 const swiper = new Swiper('.swiper', {
+  centeredSlides: true,
   speed: 400,
-  spaceBetween: 100,
-  // slidesPerView: 3,
-  // direction: getDirection,
-  
   slidesPerView: 3,
-  spaceBetween: 30,
+  spaceBetween: 8,
   // Responsive breakpoints
   breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 3,
-      spaceBetween:8
-    },
-  
     // when window width is >= 640px
     768: {
-      slidesPerView: 4,
-      spaceBetween: 32
+      spaceBetween: 32,
     },
-
-    1280: {
-      slidesPerView: 3,
-      spaceBetween: 40
-    }
   },
 
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+
   effect: 'coverflow',
   coverflowEffect: {
-    depth: 100,
+    depth: 50,
     rotate: 10,
     slideShadows: false,
-    scale: .8,
+    scale: 0.8,
   },
   loop: true,
 });
