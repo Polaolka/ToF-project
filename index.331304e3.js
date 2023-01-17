8323e03016c7e68e58ed77a00905c9142225c7d9
Polaolka <101866764+Polaolka@users.noreply.github.com>
2023-01-17 10:29:14 +0000

    Deploying to gh-pages from @ Polaolka/tof-project@b0f1b26b03ab4653612a017ca519abe92641f070 🚀

--- index.331304e3.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
-//# sourceMappingURL=index.331304e3.js.map
--- index.72806e81.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
-//# sourceMappingURL=index.72806e81.js.map
