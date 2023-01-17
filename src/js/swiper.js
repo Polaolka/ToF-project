19b03f885881de60afc9de9ac02524f94f0455f7
Olga Polishchuk <olgapolichuk@ukr.net>
2023-01-17 12:28:05 +0200

    add slider

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,12 +1,41 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
+  speed: 400,
+  spaceBetween: 100,
+  // slidesPerView: 3,
+  // direction: getDirection,
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+  slidesPerView: 3,
+  spaceBetween: 30,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 3,
+      spaceBetween:8
+    },
+  
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 32
     },
 
-  });
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  effect: 'coverflow',
+  coverflowEffect: {
+    depth: 100,
+    rotate: 10,
+    slideShadows: false,
+    scale: .8,
+  },
+  loop: true,
+});
